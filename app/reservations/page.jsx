7c591ce3c9199e7aa5028ca972315e76214eea5e
David Werth <info@davidwerth.com>
2023-09-18 17:31:09 +0200

    init reservations page

--- app/reservations/page.jsx
+++ app/reservations/page.jsx
@@ -1,9 +1,14 @@
-import UnderConstruction from '@/components/UnderConstruction';
+import BookingForm from '@/components/BookingForm';
 
 const Page = () => {
 	return (
 		<section className="flex justify-center">
-			<UnderConstruction />
+			<div className="flex flex-col items-center w-full ">
+				<h1 className="w-full py-6 text-6xl font-normal text-center text-white font-markazi bg-green">
+					Table reservation
+				</h1>
+				<BookingForm />
+			</div>
 		</section>
 	);
 };
---
+++ components/BookingForm.jsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { useState } from 'react';
+
+const BookingForm = () => {
+	const [availableTimes, setAvailableTimes] = useState([
+		'17:00',
+		'18:00',
+		'19:00',
+		'20:00',
+		'21:00',
+		'22:00',
+	]);
+
+	const [resDate, setResDate] = useState('');
+	const [resTime, setResTime] = useState('');
+	const [resGuests, setResGuests] = useState('');
+	const [resOccasion, setResOccasion] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(resDate, resTime, resGuests, resOccasion);
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className="grid justify-center w-full max-w-sm grid-cols-1 gap-8 py-10"
+		>
+			<div className="flex flex-col">
+				<label htmlFor="res-date">Choose date</label>
+				<input
+					type="date"
+					id="res-date"
+					value={resDate}
+					onChange={(e) => setResDate(e.target.value)}
+				/>
+			</div>
+
+			<div className="flex flex-col">
+				<label htmlFor="res-time">Choose time</label>
+				<select
+					id="res-time"
+					value={resTime}
+					onChange={(e) => setResTime(e.target.value)}
+				>
+					{availableTimes.map((time) => (
+						<option key={time}>{time}</option>
+					))}
+				</select>
+			</div>
+			<div className="flex flex-col">
+				<label htmlFor="guests">Number of guests</label>
+				<input
+					type="number"
+					placeholder="1"
+					min="1"
+					max="10"
+					id="guests"
+					value={resGuests}
+					onChange={(e) => setResGuests(e.target.value)}
+				/>
+			</div>
+			<div className="flex flex-col">
+				<label htmlFor="occasion">Occasion</label>
+				<select
+					id="occasion"
+					value={resOccasion}
+					onChange={(e) => setResOccasion(e.target.value)}
+				>
+					<option>Birthday</option>
+					<option>Anniversary</option>
+				</select>
+			</div>
+			<input type="submit" value="Make Your reservation" />
+		</form>
+	);
+};
+
+export default BookingForm;
