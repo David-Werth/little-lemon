229287d1e5f34739c0cd8e50897e9ecd7f1be17c
David Werth <info@davidwerth.com>
2023-09-28 06:48:32 +0200

    minor fix

--- utils/getLocalStorage.js
+++ utils/getLocalStorage.js
@@ -1,7 +1,9 @@
 const getLocalStorage = () => {
-	let data = window.localStorage.getItem('USER_CART');
-	data = JSON.parse(data);
-	return data;
+	if (typeof window !== 'undefined') {
+		let data = window.localStorage.getItem('USER_CART');
+		data = JSON.parse(data);
+		return data;
+	}
 };
 
 export default getLocalStorage;
