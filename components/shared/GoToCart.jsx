a70412c74ab8ace099e01c437fcc4381175a3663
David Werth <info@davidwerth.com>
2023-10-09 16:02:27 +0200

    style changes

--- components/shared/GoToCart.jsx
+++ components/shared/GoToCart.jsx
@@ -4,7 +4,7 @@ const GoToCart = () => {
 	return (
 		<Link
 			href="/order/cart"
-			className="fixed inset-x-0 max-w-[200px] px-4 py-3 mx-auto font-bold text-center transition-colors border-4 rounded-full bottom-4 hover:bg-green hover:border-green hover:text-white border-yellow bg-yellow text-green font-karla"
+			className="fixed inset-x-0 max-w-[200px] lg:max-w-lg px-4 py-3 mx-auto font-bold text-center transition-colors border-4 rounded-full bottom-4 hover:bg-green hover:border-green hover:text-white border-yellow bg-yellow text-green font-karla"
 		>
 			Go to cart
 		</Link>
