9f6e182d71b7ea3b02d1b3113ec2ee7732f8a96d
David Werth <info@davidwerth.com>
2023-10-12 08:16:16 +0200

    booking form to formik with yup

--- components/reservation/BookingForm.jsx
+++ components/reservation/BookingForm.jsx
@@ -1,119 +1,120 @@
 'use client';
 
-import { useState } from 'react';
+import { ErrorMessage, Field, Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
-const BookingForm = ({ availableTimes, dispatch, setFormData, submitForm }) => {
-	const [resDate, setResDate] = useState('');
-	const [resTime, setResTime] = useState(availableTimes[0]);
-	const [resGuests, setResGuests] = useState(1);
-	const [resOccasion, setResOccasion] = useState('Birthday');
-	const [formError, setFormError] = useState(false);
+const reservationDetailSchema = Yup.object().shape({
+	name: Yup.string().required('Required'),
+	date: Yup.date().required('Required'),
+	time: Yup.string().required('Required'),
+	guests: Yup.string().required('Required'),
+	occasion: Yup.string().required('Required'),
+	email: Yup.string().email('Please enter valid email').required('Required'),
+});
 
-	const handleDateChange = (e) => {
-		setResDate(e.target.value);
-		dispatch({
-			type: 'selected_date',
-			date: new Date(e.target.value),
-		});
-		setFormError(false);
-	};
-
-	const formReset = () => {
-		setResDate('');
-		setResTime(availableTimes[0]);
-		setResGuests(1);
-		setResOccasion('Birthday');
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (resDate) {
-			setFormError(false);
-			setFormData({
-				resDate,
-				resTime,
-				resGuests,
-				resOccasion,
-			});
-			submitForm();
-			formReset();
-		} else {
-			setFormError(true);
-		}
-	};
-
+const BookingForm = () => {
 	return (
-		<form
-			onSubmit={handleSubmit}
-			className="grid justify-center w-full max-w-lg grid-cols-1 gap-10 px-4 py-10 md:grid-cols-2 text-green font-karla"
+		<Formik
+			initialValues={{
+				name: '',
+				date: '',
+				time: '18:00',
+				guests: '1',
+				occasion: 'Birthday',
+				email: '',
+			}}
+			validationSchema={reservationDetailSchema}
+			onSubmit={async (values) => {}}
 		>
-			<div className="flex flex-col gap-1">
-				<label htmlFor="res-date">Choose date</label>
-				<input
-					type="date"
-					placeholder="mm/dd/yyyy"
-					id="res-date"
-					value={resDate}
-					onChange={handleDateChange}
-					className={`p-4 font-bold border rounded-md shadow-md border-green ${
-						formError && 'border-red-700 text-red-700 border-2'
-					}`}
-				/>
-			</div>
-			<div className="flex flex-col gap-1">
-				<label htmlFor="res-time">Choose time</label>
-				<select
-					id="res-time"
-					value={resTime}
-					onChange={(e) => setResTime(e.target.value)}
-					className="p-4 font-bold border rounded-md shadow-md border-green"
-				>
-					{availableTimes.map((time) => (
-						<option key={time}>{time}</option>
-					))}
-				</select>
-			</div>
-			<div className="flex flex-col gap-1">
-				<label htmlFor="guests">Number of guests</label>
-				<select
-					id="guests"
-					value={resGuests}
-					onChange={(e) => setResGuests(e.target.value)}
-					className="p-4 font-bold border rounded-md shadow-md border-green"
-				>
-					<option>1</option>
-					<option>2</option>
-					<option>3</option>
-					<option>4</option>
-					<option>5</option>
-					<option>6</option>
-					<option>7</option>
-					<option>8</option>
-					<option>9</option>
-					<option>10</option>
-				</select>
-			</div>
-			<div className="flex flex-col gap-1">
-				<label htmlFor="occasion">Occasion</label>
-				<select
-					id="occasion"
-					value={resOccasion}
-					onChange={(e) => setResOccasion(e.target.value)}
-					className="p-4 font-bold border rounded-md shadow-md border-green"
-				>
-					<option>Birthday</option>
-					<option>Anniversary</option>
-				</select>
-			</div>
-			<div className="text-center md:col-span-2">
-				{formError && <p className="text-red-700">Please choose a date!</p>}
-				<input
-					type="submit"
-					value="Make Your reservation"
-					className="px-4 py-3 font-bold transition-colors border-4 rounded-full cursor-pointer hover:bg-green hover:border-green hover:text-white border-yellow bg-yellow"
-				/>
-			</div>
-		</form>
+			<Form className="grid justify-center w-full max-w-lg grid-cols-1 gap-10 px-4 py-10 md:grid-cols-2 text-green font-karla">
+				<div className="flex flex-col gap-1">
+					<label htmlFor="name">Your name</label>
+					<Field
+						type="text"
+						name="name"
+						placeholder="John Doe"
+						autoComplete="off"
+						className="p-4 font-bold border rounded-md border-green"
+					/>
+					<ErrorMessage name="name" />
+				</div>
+				<div className="flex flex-col gap-1">
+					<label htmlFor="date">Choose date</label>
+					<Field
+						type="date"
+						placeholder="mm/dd/yyyy"
+						name="date"
+						className="p-4 font-bold border rounded-md border-green"
+					/>
+					<ErrorMessage name="date" />
+				</div>
+				<div className="flex flex-col gap-1">
+					<label htmlFor="time">Choose time</label>
+					<Field
+						as="select"
+						name="time"
+						className="p-4 font-bold border rounded-md border-green"
+					>
+						<option>18:00</option>
+						<option>18:30</option>
+						<option>19:00</option>
+					</Field>
+					<ErrorMessage name="time" />
+				</div>
+				<div className="flex flex-col gap-1">
+					<label htmlFor="guests">Number of guests</label>
+					<Field
+						as="select"
+						name="guests"
+						className="p-4 font-bold border rounded-md border-green"
+					>
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+					</Field>
+					<ErrorMessage name="guests" />
+				</div>
+				<div className="flex flex-col gap-1">
+					<label htmlFor="occasion">Occasion</label>
+					<Field
+						as="select"
+						name="occasion"
+						className="p-4 font-bold border rounded-md border-green"
+					>
+						<option>Birthday</option>
+						<option>Anniversary</option>
+						<option>Other</option>
+					</Field>
+					<ErrorMessage name="occasion" />
+				</div>
+				<div className="flex flex-col gap-1">
+					<label htmlFor="name">Email address</label>
+					<Field
+						type="text"
+						name="email"
+						placeholder="You may enter a fake email"
+						autoComplete="off"
+						className="p-4 font-bold border rounded-md border-green"
+					/>
+					<ErrorMessage name="email" />
+				</div>
+				<div className="text-center md:col-span-2">
+					<button
+						type="submit"
+						className="px-4 py-3 font-bold text-center transition-colors border-4 rounded-full cursor-pointer hover:bg-green hover:border-green hover:text-white border-yellow bg-yellow text-green font-karla"
+					>
+						Make Your reservation
+					</button>
+				</div>
+			</Form>
+		</Formik>
 	);
 };
 
