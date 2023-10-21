9fcd2cd6e465915db2fadc9a496c04bf1921441d
David Werth <info@davidwerth.com>
2023-10-21 18:50:59 +0200

    scrollToTop only visible after 400px scroll

--- components/shared/ScrollToTop.jsx
+++ components/shared/ScrollToTop.jsx
@@ -2,8 +2,9 @@
 
 import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
 
-const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
+const isBrowser = () => typeof window !== 'undefined';
 
 function scrollToTop() {
 	if (!isBrowser()) return;
@@ -11,12 +12,27 @@ function scrollToTop() {
 }
 
 const ScrollToTop = () => {
+	const [showBtn, setShowBtn] = useState(false);
+
+	useEffect(() => {
+		if (!isBrowser()) return;
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 400) {
+				setShowBtn(true);
+			} else {
+				setShowBtn(false);
+			}
+		});
+	}, []);
+
 	return (
 		<div className="fixed z-50 cursor-pointer bottom-4 right-4">
 			<FontAwesomeIcon
 				icon={faArrowCircleUp}
 				onClick={scrollToTop}
-				className="h-12 border-2 rounded-full bg-yellow text-green"
+				className={`h-12 border-2 rounded-full bg-yellow text-green ${
+					showBtn ? 'opacity-100' : 'opacity-0'
+				} transition-opacity`}
 			/>
 		</div>
 	);
