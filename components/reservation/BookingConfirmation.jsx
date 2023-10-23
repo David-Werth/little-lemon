b6a79089f94daf62a60e236f5538abbce422f82d
David Werth <info@davidwerth.com>
2023-10-23 06:42:11 +0200

    logic bug fix

--- components/reservation/BookingConfirmation.jsx
+++ components/reservation/BookingConfirmation.jsx
@@ -6,8 +6,8 @@ const BookingConfirmation = ({ formData }) => {
 			</h2>
 			<p className="text-xl font-karla">
 				Thank you for booking a table for {formData.guests}! <br /> See you on{' '}
-				{new Date(formData.date).toDateString()} at {formData.time} for your{' '}
-				{formData.occasion}.
+				{new Date(formData.date).toDateString()} at {formData.time}
+				{formData.occasion != 'Other' ? `for your ${formData.occasion}` : ''}.
 			</p>
 		</div>
 	);
